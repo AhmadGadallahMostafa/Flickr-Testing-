014f87e8f6afbc6175feedf1a3ea8eb5728a9073
KarimAmrM <59086314+KarimAmrM@users.noreply.github.com>
2021-05-04 01:30:30 +0200

    Added cancelled upload test and switched to self.assert

--- locator.py
+++ locator.py
@@ -14,16 +14,19 @@ class LoginPageLocators(object):
 class HomePageLocators(object):
 
     UPLOAD_ICON = (By.CLASS_NAME, "upload-icon")
+    YOU = (By.XPATH, "//ul[@class='nav-menu desktop-only']//li[1]")
 
 
 class UploadPageLocators(object):
 
-    CHOOSE_PHOTO = (By.ID, "choose-photos-and-videos")
+    CHOOSE_PHOTO = (By.ID, "button-add-photos")
     DIALOG_BOX = (By.ID, "editr-publish-dialog")
     PUBLISH = (By.ID, "action-publish")
     CONFRIM_PUBLISH = (By.ID, "confirm-publish")
     USER_MESSAGING = (By.ID, "user-messaging")
     REMOVE_FILE_BUTTON = (By.ID, "action-remove-errors")
+    PHOTOSTREAM_LINK = (By.CLASS_NAME, "gn-link")
+    UPLOAD_BOX = (By.CLASS_NAME, "selected-wrapper")
 
 
 class PhotoStreamLocators(object):
