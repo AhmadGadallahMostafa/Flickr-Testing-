f755845746bbe15c0e66ae6a43429c61bc0fe160
mohamed886 <61754682+mohamed886@users.noreply.github.com>
2021-06-08 15:18:06 +0200

    added comments to website

--- Website-testing/Pages/AboutPage.py
+++ Website-testing/Pages/AboutPage.py
@@ -7,167 +7,168 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 import time
 class AboutPage(BasePage):
+    #In each function of the following after reaching the about page we click the edit profile info button, then we choose the required textbox to update, then we check that the info is updated
     def edit_occupation(self):
-        edit = self.driver.find_element(*AboutPageLocators.EDIT)
+        edit = self.driver.find_element(*AboutPageLocators.EDIT)  #Locating the edit button
         self.driver.execute_script("window.scrollTo(0, 750)")
         time.sleep(2)
         edit.click()
         time.sleep(1)
-        occupation = self.driver.find_element(*AboutPageLocators.OCCUPATION_TEXTBOX)
-        occupation.clear()
+        occupation = self.driver.find_element(*AboutPageLocators.OCCUPATION_TEXTBOX)  #Locating the occupation textbox
+        occupation.clear()  #clearing the occupation textbox
         time.sleep(1)
-        occupation.send_keys("Engineer")
+        occupation.send_keys("Engineer")  #Edit the occupation
         time.sleep(5)
-        done = self.driver.find_element(*AboutPageLocators.DONE)
+        done = self.driver.find_element(*AboutPageLocators.DONE)  #Locating the done button
         done.click()
         time.sleep(5)
-        occupation_info = self.driver.find_element(*AboutPageLocators.OCCUPATION_INFO)
-        result = "Engineer" in occupation_info.text
+        occupation_info = self.driver.find_element(*AboutPageLocators.OCCUPATION_INFO)  #Locating the occupation info
+        result = "Engineer" in occupation_info.text  #Checking that the info is updated
         return result
 
     def edit_home_town(self):
-        edit = self.driver.find_element(*AboutPageLocators.EDIT)
+        edit = self.driver.find_element(*AboutPageLocators.EDIT)  #Locating the edit button
         self.driver.execute_script("window.scrollTo(0, 750)")
         time.sleep(2)
         edit.click()
         time.sleep(1)
-        home_town = self.driver.find_element(*AboutPageLocators.HOME_TOWN_TEXTBOX)
-        home_town.clear()
+        home_town = self.driver.find_element(*AboutPageLocators.HOME_TOWN_TEXTBOX)  #Locating the home town textbox
+        home_town.clear()  #clearing the textbox
         time.sleep(1)
-        home_town.send_keys("Giza")
+        home_town.send_keys("Giza")  #Edit the home town
         time.sleep(5)
-        done = self.driver.find_element(*AboutPageLocators.DONE)
+        done = self.driver.find_element(*AboutPageLocators.DONE)  #Locating the done button
         done.click()
         time.sleep(3)
-        home_town_info = self.driver.find_element(*AboutPageLocators.HOME_TOWN_INFO)
+        home_town_info = self.driver.find_element(*AboutPageLocators.HOME_TOWN_INFO)  #Locating the home town info
         print(home_town_info.text)
-        result = "Giza" in home_town_info.text
+        result = "Giza" in home_town_info.text  #Checking that the info is updated
         return result
 
     def edit_city(self):
-        edit = self.driver.find_element(*AboutPageLocators.EDIT)
+        edit = self.driver.find_element(*AboutPageLocators.EDIT)  #Locating the edit button
         self.driver.execute_script("window.scrollTo(0, 750)")
         time.sleep(2)
         edit.click()
         time.sleep(1)
-        city = self.driver.find_element(*AboutPageLocators.CITY_TEXTBOX)
-        city.clear()
+        city = self.driver.find_element(*AboutPageLocators.CITY_TEXTBOX)  #Locating the city textbox
+        city.clear()  #clearing the textbox
         time.sleep(1)
-        city.send_keys("Giza")
+        city.send_keys("Giza")  #Edit the city
         time.sleep(5)
-        done = self.driver.find_element(*AboutPageLocators.DONE)
+        done = self.driver.find_element(*AboutPageLocators.DONE)  #Locating the done button
         done.click()
         time.sleep(3)
-        city_info = self.driver.find_element(*AboutPageLocators.CITY_INFO)
-        result = "Giza" in city_info.text
+        city_info = self.driver.find_element(*AboutPageLocators.CITY_INFO)  #Locating the city info
+        result = "Giza" in city_info.text  #Checking that the info is updated
         return result
 
     def edit_country(self):
-        edit = self.driver.find_element(*AboutPageLocators.EDIT)
+        edit = self.driver.find_element(*AboutPageLocators.EDIT)  #Locating the edit button
         self.driver.execute_script("window.scrollTo(0, 750)")
         time.sleep(2)
         edit.click()
         time.sleep(1)
-        country = self.driver.find_element(*AboutPageLocators.COUNTRY_TEXTBOX)
-        country.clear()
+        country = self.driver.find_element(*AboutPageLocators.COUNTRY_TEXTBOX)  #Locating the country textbox
+        country.clear()  #clearing the textbox
         time.sleep(1)
-        country.send_keys("Egypt")
+        country.send_keys("Egypt")  #Edit the country
         time.sleep(5)
-        done = self.driver.find_element(*AboutPageLocators.DONE)
+        done = self.driver.find_element(*AboutPageLocators.DONE)  #Locating the done button
         done.click()
         time.sleep(3)
-        country_info = self.driver.find_element(*AboutPageLocators.COUNTRY_INFO)
-        result = "Egypt" in country_info.text
+        country_info = self.driver.find_element(*AboutPageLocators.COUNTRY_INFO)  #Locating the country info
+        result = "Egypt" in country_info.text  #Checking that the info is updated
         return result
 
     def edit_facebook(self):
-        edit = self.driver.find_element(*AboutPageLocators.EDIT)
+        edit = self.driver.find_element(*AboutPageLocators.EDIT)  #Locating the edit button
         self.driver.execute_script("window.scrollTo(0, 750)")
         time.sleep(2)
         edit.click()
         time.sleep(1)
-        facebook = self.driver.find_element(*AboutPageLocators.FACEBOOK_TEXTBOX)
-        facebook.clear()
+        facebook = self.driver.find_element(*AboutPageLocators.FACEBOOK_TEXTBOX)  #Locating the facebook textbox
+        facebook.clear()  #clearing the textbox
         time.sleep(1)
-        facebook.send_keys("MohamedAmr")
+        facebook.send_keys("MohamedAmr")  #Edit the facebook info
         time.sleep(5)
-        done = self.driver.find_element(*AboutPageLocators.DONE)
+        done = self.driver.find_element(*AboutPageLocators.DONE)  #Locating the done button
         done.click()
         time.sleep(3)
-        facebook_info = self.driver.find_element(*AboutPageLocators.FACEBOOK_INFO)
-        result = "MohamedAmr" in facebook_info.text
+        facebook_info = self.driver.find_element(*AboutPageLocators.FACEBOOK_INFO)  #Locating the facebook info
+        result = "MohamedAmr" in facebook_info.text  #Checking that the info is updated
         return result
 
     def edit_twitter(self):
-        edit = self.driver.find_element(*AboutPageLocators.EDIT)
+        edit = self.driver.find_element(*AboutPageLocators.EDIT)  #Locating the edit button
         self.driver.execute_script("window.scrollTo(0, 750)")
         time.sleep(2)
         edit.click()
         time.sleep(1)
-        twitter = self.driver.find_element(*AboutPageLocators.TWITTER_TEXTBOX)
-        twitter.clear()
+        twitter = self.driver.find_element(*AboutPageLocators.TWITTER_TEXTBOX)  #Locating the twitter textbox
+        twitter.clear()  #clearing the textbox
         time.sleep(1)
-        twitter.send_keys("MohamedAmr1")
+        twitter.send_keys("MohamedAmr1")   #Edit the twitter info
         time.sleep(5)
-        done = self.driver.find_element(*AboutPageLocators.DONE)
+        done = self.driver.find_element(*AboutPageLocators.DONE)  #Locating the done button
         done.click()
         time.sleep(3)
-        twitter_info = self.driver.find_element(*AboutPageLocators.TWITTER_INFO)
-        result = "MohamedAmr1" in twitter_info.text
+        twitter_info = self.driver.find_element(*AboutPageLocators.TWITTER_INFO)  #Locating the twitter info
+        result = "MohamedAmr1" in twitter_info.text  #Checking that the info is updated
         return result
 
     def edit_instagram(self):
-        edit = self.driver.find_element(*AboutPageLocators.EDIT)
+        edit = self.driver.find_element(*AboutPageLocators.EDIT)  #Locating the edit button
         self.driver.execute_script("window.scrollTo(0, 750)")
         time.sleep(2)
         edit.click()
         time.sleep(1)
-        instagram = self.driver.find_element(*AboutPageLocators.INSTAGRAM_TEXTBOX)
-        instagram.clear()
+        instagram = self.driver.find_element(*AboutPageLocators.INSTAGRAM_TEXTBOX)  #Locating the instagram textbox
+        instagram.clear()  #clearing the textbox
         time.sleep(1)
-        instagram.send_keys("MohamedAmr2")
+        instagram.send_keys("MohamedAmr2")  #Edit the instagram info
         time.sleep(5)
-        done = self.driver.find_element(*AboutPageLocators.DONE)
+        done = self.driver.find_element(*AboutPageLocators.DONE)  #Locating the done button
         done.click()
         time.sleep(3)
-        instagram_info = self.driver.find_element(*AboutPageLocators.INSTAGRAM_INFO)
-        result = "MohamedAmr2" in instagram_info.text
+        instagram_info = self.driver.find_element(*AboutPageLocators.INSTAGRAM_INFO)  #Locating the instagram info
+        result = "MohamedAmr2" in instagram_info.text  #Checking that the info is updated
         return result
 
     def edit_pinterest(self):
-        edit = self.driver.find_element(*AboutPageLocators.EDIT)
+        edit = self.driver.find_element(*AboutPageLocators.EDIT)  #Locating the edit button
         self.driver.execute_script("window.scrollTo(0, 750)")
         time.sleep(2)
         edit.click()
         time.sleep(1)
-        pinterest = self.driver.find_element(*AboutPageLocators.PINTEREST_TEXTBOX)
-        pinterest.clear()
+        pinterest = self.driver.find_element(*AboutPageLocators.PINTEREST_TEXTBOX)  #Locating the pinterest textbox
+        pinterest.clear()  #clearing the textbox
         time.sleep(1)
-        pinterest.send_keys("MohamedAmr3")
+        pinterest.send_keys("MohamedAmr3")  #Edit the pinterest info
         time.sleep(5)
-        done = self.driver.find_element(*AboutPageLocators.DONE)
+        done = self.driver.find_element(*AboutPageLocators.DONE)  #Locating the done button
         done.click()
         time.sleep(3)
-        pinterest_info = self.driver.find_element(*AboutPageLocators.PINTEREST_INFO)
-        result = "MohamedAmr3" in pinterest_info.text
+        pinterest_info = self.driver.find_element(*AboutPageLocators.PINTEREST_INFO)  #Locating the pinterest info
+        result = "MohamedAmr3" in pinterest_info.text  #Checking that the info is updated
         return result
 
     def edit_tumblr(self):
-        edit = self.driver.find_element(*AboutPageLocators.EDIT)
+        edit = self.driver.find_element(*AboutPageLocators.EDIT)  #Locating the edit button
         self.driver.execute_script("window.scrollTo(0, 750)")
         time.sleep(2)
         edit.click()
         time.sleep(1)
-        tumblr = self.driver.find_element(*AboutPageLocators.TUMBLR_TEXTBOX)
-        tumblr.clear()
+        tumblr = self.driver.find_element(*AboutPageLocators.TUMBLR_TEXTBOX)  #Locating the tumblr textbox
+        tumblr.clear()  #clearing the textbox
         time.sleep(1)
-        tumblr.send_keys("MohamedAmr4")
+        tumblr.send_keys("MohamedAmr4")  #Edit the tumblr info
         time.sleep(5)
-        done = self.driver.find_element(*AboutPageLocators.DONE)
+        done = self.driver.find_element(*AboutPageLocators.DONE)   #Locating the done button
         done.click()
         time.sleep(3)
-        tumblr_info = self.driver.find_element(*AboutPageLocators.TUMBLR_INFO)
-        result = "MohamedAmr4" in tumblr_info.text
+        tumblr_info = self.driver.find_element(*AboutPageLocators.TUMBLR_INFO)  #Locating the tumblr info
+        result = "MohamedAmr4" in tumblr_info.text  #Checking that the info is updated
         return result
 
 
--- Website-testing/Pages/PhotoViewPage.py
+++ Website-testing/Pages/PhotoViewPage.py
@@ -6,16 +6,17 @@ from selenium import webdriver
 import time
 
 class PhotoViewPage(BasePage):
+    #In this function we locate the comment textbox then write a comment
     def comment(self):
-        comment = self.driver.find_element(*PhotoViewLocators.COMMENT_TEXTBOX)
-        comment.send_keys("Nice pic")
+        comment = self.driver.find_element(*PhotoViewLocators.COMMENT_TEXTBOX)  #Locating the comment textbox
+        comment.send_keys("Nice pic")  #Write a comment
         time.sleep(3)
-        comment_button = self.driver.find_element(*PhotoViewLocators.COMMENT_BUTTON)
+        comment_button = self.driver.find_element(*PhotoViewLocators.COMMENT_BUTTON)  #Locating the comment button
         comment_button.click()
         time.sleep(3)
-
+    #After executing the above function we check that the comment is written
     def check_comment(self):
-        comment = self.driver.find_element(*PhotoViewLocators.COMMENT_TEXT)
-        result = "Nice pic" in comment.text
+        comment = self.driver.find_element(*PhotoViewLocators.COMMENT_TEXT)  #Locating the comment textbox
+        result = "Nice pic" in comment.text  #Checking that the comment is written
         return result
 
